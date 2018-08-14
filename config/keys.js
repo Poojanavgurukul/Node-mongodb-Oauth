81dae989a4dd0f1603c6271e276dc0300c0500b0
poojanavgurukul <pooja17@navgurukul.org>
2018-08-15 04:00:00 +0530

    oauth through google

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+	google:{
+		clientID:'1097848919829-9i5in8d46o3ft1gk0lldukn1jdc641a3.apps.googleusercontent.com',
+		clientSecret:'rjYqUKG70deZZqgWsu0bK_lY',
+	},
+	mongodb:{
+		dbURI:'mongodb://pooja:pooja17navgurukul@ds121652.mlab.com:21652/soal-testing'
+	},
+	session:{
+		cookieKey:'pooja17navgurukul'
+	}
+};
